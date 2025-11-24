3b1312407a90995434f3985b4247b6c99bfa87ba
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-23 19:06:44 -0600

    :wrench: Update general configs in project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import envVariables from '@Config/env-variables';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const vars = envVariables();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(vars.port);
+  console.log(`Server is running on port ${vars.port}`);
 }
 bootstrap();
