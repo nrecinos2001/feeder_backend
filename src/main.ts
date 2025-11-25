f05edc3b9ecbf5556df0fd2e93faf44bb200bc3f
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-25 07:56:03 -0600

    :wrench: Update validation pipies config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 import { envVariables } from '@Config/env-variables';
+
+import { AppModule } from './app.module';
 
 const vars = envVariables();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(vars.port);
   app.enableCors();
   console.log(`Server is running on port ${vars.port}`);
