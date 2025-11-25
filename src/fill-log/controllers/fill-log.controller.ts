f94fc6162ec9a300fa5d2f320c3210a63cfcb125
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-24 23:41:07 -0600

    :sparkles: Add fill log endpoint

--- src/fill-log/controllers/fill-log.controller.ts
+++ src/fill-log/controllers/fill-log.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+
+import { FillLogService } from '@FillLog/services/fill-log.service';
 
 @Controller('fill-log')
-export class FillLogController {}
+export class FillLogController {
+  constructor(private readonly fillLogService: FillLogService) {}
+  @Get()
+  async getFillLogs() {
+    return await this.fillLogService.getFillLogs();
+  }
+}
--- src/fill-log/services/fill-log.service.ts
+++ src/fill-log/services/fill-log.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { FillLogRepository } from '@FillLog/repositories/fill-log.repository';
+import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FillLogService {}
+export class FillLogService {
+  constructor(
+    @Inject('FILL_LOG_REPOSITORY')
+    private readonly fillLogRepository: FillLogRepository,
+  ) {}
+
+  async getFillLogs() {
+    return await this.fillLogRepository.findOrderByCreatedAtDesc();
+  }
+}
