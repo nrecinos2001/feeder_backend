76b11ed288d9c00dff257dd4a9bd090fef1eede5
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-29 09:56:42 -0600

    :sparkles: Add flow to create and get schedules

--- src/feed-schedule/controllers/feed-schedule.controller.ts
+++ src/feed-schedule/controllers/feed-schedule.controller.ts
@@ -1,11 +1,23 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
 import { FeedScheduleService } from '../services';
-import { Controller, Get } from '@nestjs/common';
+import { CreateFeedScheduleDto } from '../dtos';
+import { FeedSchedulerDoc } from '../docs';
 
 @Controller('feed-schedule')
 export class FeedScheduleController {
   constructor(private readonly feedScheduleService: FeedScheduleService) {}
   @Get()
-  async getFeedSchedules() {
+  async getFeedSchedules(): Promise<FeedSchedulerDoc[]> {
     return await this.feedScheduleService.getFeedSchedules();
+  }
+
+  @Post()
+  async createFeedSchedules(
+    @Body() createFeedScheduleDto: CreateFeedScheduleDto,
+  ): Promise<FeedSchedulerDoc> {
+    return await this.feedScheduleService.createFeedSchedules(
+      createFeedScheduleDto,
+    );
   }
 }
--- src/feed-schedule/services/feed-schedule.service.ts
+++ src/feed-schedule/services/feed-schedule.service.ts
@@ -1,14 +1,42 @@
 import { Inject, Injectable } from '@nestjs/common';
+import dayjs from 'dayjs';
+
+import { serializeDocClass } from '@Commons/utils';
 
 import { FeedScheduleRepository } from '../repositories';
+import { CreateFeedScheduleDto } from '../dtos';
+import { FeedSchedulerDoc } from '../docs';
 
 @Injectable()
 export class FeedScheduleService {
   constructor(
     @Inject('FEED_SCHEDULE_REPOSITORY')
     private readonly feedScheduleRepository: FeedScheduleRepository,
-  ) { }
-  async getFeedSchedules() {
+  ) {}
+  async getFeedSchedules(): Promise<FeedSchedulerDoc[]> {
     const feedSchedules = await this.feedScheduleRepository.find();
+    return serializeDocClass<FeedSchedulerDoc[]>(
+      FeedSchedulerDoc,
+      feedSchedules,
+    );
+  }
+
+  async createFeedSchedules(
+    createFeedScheduleDto: CreateFeedScheduleDto,
+  ): Promise<FeedSchedulerDoc> {
+    const utcDate = new Date(createFeedScheduleDto.date.toISOString());
+    const hour = dayjs(utcDate).hour().toString();
+    const minute = dayjs(utcDate).minute().toString();
+
+    const newFeedSchedule = this.feedScheduleRepository.create({
+      hour,
+      minute,
+    });
+    const savedFeedSchedule =
+      await this.feedScheduleRepository.save(newFeedSchedule);
+    return serializeDocClass<FeedSchedulerDoc>(
+      FeedSchedulerDoc,
+      savedFeedSchedule,
+    );
   }
 }
