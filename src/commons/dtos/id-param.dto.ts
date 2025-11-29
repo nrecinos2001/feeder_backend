b57bfd34c2a6ee1155c9e61289c600a904384c30
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-29 15:56:53 -0600

    :bug: Update transform to delete items

--- src/commons/dtos/id-param.dto.ts
+++ src/commons/dtos/id-param.dto.ts
@@ -1,7 +1,9 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class IdParamDto {
   @IsNumber()
   @IsNotEmpty()
+  @Transform(({ value }) => Number(value))
   id: number;
 }
