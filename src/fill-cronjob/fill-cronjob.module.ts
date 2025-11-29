c2cc991a1420fee748faa8ce284fa48e39a777fe
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-29 17:11:55 -0600

    :sparkles: Add flow to manage hours to run feeder automatically

--- src/fill-cronjob/fill-cronjob.module.ts
+++ src/fill-cronjob/fill-cronjob.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { FeedScheduleModule } from '@FeedSchedule/feed-schedule.module';
+
 import { FillCronjobService } from './services';
 
 @Module({
+  imports: [FeedScheduleModule],
   providers: [FillCronjobService],
   exports: [FillCronjobService],
 })
--- src/fill-cronjob/services/fill-cronjob.service.ts
+++ src/fill-cronjob/services/fill-cronjob.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import * as cron from 'node-cron';
+import dayjs from 'dayjs';
+import { FeedScheduleService } from '@FeedSchedule/services';
 
 @Injectable()
-export class FillCronjobService {}
+export class FillCronjobService {
+  constructor(private readonly feedScheduleService: FeedScheduleService) {
+    this.scheduleFillJob();
+  }
+  scheduleFillJob() {
+    cron.schedule('* * * * *', async () => {
+      const dayjsNow = dayjs();
+      const hour = dayjsNow.format('H');
+      const minute = dayjsNow.format('m');
+      const schedulers = await this.feedScheduleService.getFeedSchedules();
+      const existingScheduler = schedulers.find((scheduler) => {
+        return scheduler.hour === hour && scheduler.minute === minute;
+      });
+      if (!existingScheduler) {
+        return;
+      }
+      console.log(`Cronjob executed at ${hour}:${minute}`);
+      // Add your fill logic here
+    });
+  }
+}
