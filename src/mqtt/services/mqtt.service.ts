4f49fde21629bbea3f45c2405dbb292b09556a96
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-29 21:56:59 -0600

    :sparkles: Add events for mqtt

--- src/mqtt/services/mqtt.service.ts
+++ src/mqtt/services/mqtt.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { connect, MqttClient } from 'mqtt';
 
+import { envVariables } from '@Config/env-variables';
+
+const vars = envVariables();
 @Injectable()
-export class MqttService {}
+export class MqttService {
+  private client: MqttClient;
+
+  onModuleInit() {
+    this.client = connect(vars.mqttServer);
+
+    this.client.on('connect', () => {
+      console.log('MQTT conectado');
+
+      this.client.subscribe('petfeeder/servo/status');
+      this.client.subscribe('petfeeder/distance');
+    });
+
+    this.client.on('message', (topic, message) => {
+      const msg = message.toString();
+
+      if (topic === 'petfeeder/servo/status') {
+        console.log('Servo status:', msg);
+      }
+
+      if (topic === 'petfeeder/distance') {
+        console.log('Alerta de distancia:', msg);
+      }
+    });
+  }
+
+  sendOpenServo() {
+    this.client.publish('petfeeder/servo/open', 'open');
+    console.log('Published: open to petfeeder/servo/open');
+  }
+}
