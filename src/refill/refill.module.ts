c382184764c43d2633c9e4e09d052714c6f1f052
Nestor Recinos <nestor.recinos2001@gmail.com>
2025-11-30 19:58:24 -0600

    :sparkles: Add method to get last N refill logs

--- src/refill/refill.module.ts
+++ src/refill/refill.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
+import { DatabaseModule } from '@Database/database.module';
+
+import { RefillRepository } from './repositories';
 import { RefillService } from './services';
 
 @Module({
-  providers: [RefillService],
+  imports: [DatabaseModule],
+  providers: [
+    RefillService,
+    {
+      provide: 'REFILL_REPOSITORY',
+      inject: ['DATA_SOURCE'],
+      useFactory: (dataSource: DataSource) => RefillRepository(dataSource),
+    },
+  ],
+  exports: ['REFILL_REPOSITORY', RefillService],
 })
 export class RefillModule {}
--- src/refill/services/refill.service.ts
+++ src/refill/services/refill.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { RefillEntity } from '@Refill/entities';
+import { RefillRepository } from '@Refill/repositories';
 
 @Injectable()
-export class RefillService {}
+export class RefillService {
+  constructor(
+    @Inject('REFILL_REPOSITORY')
+    private readonly refillRepository: RefillRepository,
+  ) { }
+  async getLastNRefills(n: number): Promise<RefillEntity[]> {
+    const refillLogs = await this.refillRepository.findLastNFills(n);
+    return refillLogs;
+  }
+}
